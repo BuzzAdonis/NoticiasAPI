a30be8049d80363df0cf6bd9dbd2c9ef01df8dee
BuzzAdonis <55252048+BuzzAdonis@users.noreply.github.com>
2022-03-02 22:25:51 -0400

    Mapeo completado

---
+++ NoticiasAPI/Models/Autor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoticiasAPI.Models
+{
+    class Autor
+    {
+        public int NoticiaID { get; set; }
+        public int AutorID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaDeCreacion { get; set; }
+        public DateTime FechaDeEdicion { get; set; }
+    }
+}
--- NoticiasAPI/Models/Noticia.cs
+++ NoticiasAPI/Models/Noticia.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace NoticiasAPI.Models
 {
     class Noticia
     {
-        public int MyProperty { get; set; }
+        public int NoticiaID { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion  { get; set; }
+        public string Contenido { get; set; }
+        public DateTime FechaDeCreacion { get; set; }
+        public DateTime FechaDeEdicion { get; set; }
+        public int AutorID { get; set; }
+        public Autor Autor { get; set; }
+        
+        public class Mapeo
+        {
+            public Mapeo(EntityTypeBuilder<Noticia> mepeoNoticia)
+            {
+                mepeoNoticia.HasKey(x => x.NoticiaID);
+               mepeoNoticia.Property(x => x.Titulo).HasColumnName("Titulo");
+                mepeoNoticia.ToTable("Noticia");
+                mepeoNoticia.HasOne(x =>x.Autor);
+            }
+        }
     }
 }
